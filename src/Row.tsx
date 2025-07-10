1e73f53b685b19a9fa4b14e9e60dacc90f7c5867
Nicholas McMaster <nmcmaster@gmail.com>
2025-07-10 13:00:11 -0400

    complete show list

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,27 +1,30 @@
 export default function Row({
-	venue,
-	date,
-	location,
-	link,
-	time,
+    venue,
+    date,
+    location,
+    link,
+    time,
 }: {
-	venue: string;
-	date: string;
-	location: string;
-	time?: string;
-	flyer?: string;
-	link?: string;
+    venue: string;
+    date: string;
+    location: string;
+    time?: string;
+    flyer?: string;
+    link?: string;
+    withBI?: boolean;
+    ambient?: boolean;
 }) {
-
-	return (
-		<div className="flex text-lg text-gray-50 w-3/4 mx-auto justify-between p-4 bg-stone-900 rounded-lg">
-			<div>
-				<div>{venue}</div>
-				<div>{location}</div>
-        <div>{link}</div>
-      
-			</div>
-	<div>	<div>{date}</div>  <div>{time}</div></div>	
-		</div>
-	);
+    return (
+        <div className="flex text-lg text-gray-50 w-3/4 mx-auto justify-between p-4 bg-stone-900 rounded-lg">
+            <div>
+                <div>{venue}</div>
+                <div>{location}</div>
+                <div>{link}</div>
+            </div>
+            <div>
+                {" "}
+                <div>{date}</div> <div>{time}</div>
+            </div>
+        </div>
+    );
 }
